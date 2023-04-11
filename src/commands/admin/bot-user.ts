d25965cd6bda2584420e28206e80e74105b9e3e2
kida31 <83455653+kida31@users.noreply.github.com>
2023-04-11 20:04:24 +0200

    Add "forget" command

--- src/commands/admin/bot-user.ts
+++ src/commands/admin/bot-user.ts
@@ -1,3 +1,4 @@
+import { ExtendedClient } from "../../core/extended-client";
 import { Command, CommandContext, isMessage } from "../command";
 
 export default [
@@ -29,6 +30,26 @@ export default [
                     await context.client.user.setAvatar(url);
                     await context.react("✅");
                 }
+            }
+        }
+    },
+
+    class ForgetChatCommand implements Command {
+        name: string = "forget";
+        alias?: string[] | undefined;
+        description?: string | undefined;
+        category?: string | undefined;
+        // data?: Jsonable | Jsonable[] | undefined;
+        async execute(context: CommandContext): Promise<void> {
+            const chatbot = (<ExtendedClient>context.client).chatbot;
+            if (chatbot) {
+                for(let i = 0; i < 20; i++) {
+                    await chatbot.read("\n");
+                }
+            }
+
+            if (isMessage(context)) {
+                await context.react("✅");
             }
         }
     }
